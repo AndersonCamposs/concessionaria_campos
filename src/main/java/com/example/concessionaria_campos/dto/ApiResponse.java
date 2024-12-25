ed820c3bac2cebe73a1fb75b2e7efa765110f4d6
Anderson Campos <anderson.camposs87@outlook.com>
2024-12-25 00:10:34 -0300

    Correções pontuais

--- src/main/java/com/example/concessionaria_campos/dto/ApiResponse.java
+++ src/main/java/com/example/concessionaria_campos/dto/ApiResponse.java
@@ -1,10 +1,12 @@
 package com.example.concessionaria_campos.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+@Schema(description = "Detalhes de uma resposta personalizada")
 @Getter
 @Setter
 public class ApiResponse {
