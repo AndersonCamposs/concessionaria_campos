23a2de8ca2cc8309554d8450b1285ce7cf734c19
Anderson Campos <anderson.camposs87@outlook.com>
2025-01-26 22:18:33 -0300

    Adicionando uma nova herança ao Repository
    Estendendo JpaSpecificationExecutor para execução buscas com especificações

--- src/main/java/com/example/concessionaria_campos/repository/VehicleRepository.java
+++ src/main/java/com/example/concessionaria_campos/repository/VehicleRepository.java
@@ -2,10 +2,10 @@ package com.example.concessionaria_campos.repository;
 
 import com.example.concessionaria_campos.entity.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.Optional;
 
-public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
-
+public interface VehicleRepository extends JpaRepository<Vehicle, Long>, JpaSpecificationExecutor<Vehicle> {
     Optional<Vehicle> findByPlate(String plate);
 }
